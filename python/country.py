1e336976c47f9d453956328214539e40a3fe934a
Elián Hanisch <lambdae2@gmail.com>
2009-08-18 22:23:20 -0300

    country.py
    /country accepts nicks as argument

--- python/country.py
+++ python/country.py
@@ -2,6 +2,10 @@
 
 SCRIPT_NAME    = "country"
 SCRIPT_AUTHOR  = "Elián Hanisch <lambdae2@gmail.com>"
+SCRIPT_VERSION = "0.1"
+SCRIPT_LICENSE = "GPL3"
+SCRIPT_DESC    = ""
+SCRIPT_COMMAND = "country"
 
 try:
 	import weechat
@@ -14,9 +18,11 @@ except ImportError:
 
 import os
 
+### ip database
 database_url = 'http://geolite.maxmind.com/download/geoip/database/GeoIPCountryCSV.zip'
 database_file = 'GeoIPCountryWhois.csv'
 
+### messages
 def say(s, prefix=SCRIPT_NAME, buffer=''):
 	weechat.prnt(buffer, '%s: %s' %(prefix, s))
 
@@ -36,12 +42,19 @@ def whois(s, nick, buffer=''):
 			weechat.color('chat'),
 			s))
 
+### functions
 def get_script_dir():
 	script_dir = weechat.info_get('weechat_dir', '')
 	script_dir = os.path.join(script_dir, 'country')
 	if not os.path.isdir(script_dir):
 		os.makedirs(script_dir)
 	return script_dir
+
+ip_database = ''
+def check_database():
+	global ip_database
+	ip_database = os.path.join(get_script_dir(), database_file)
+	return os.path.isfile(ip_database)
 
 timeout = 1000*60
 hook_download = ''
@@ -55,35 +68,37 @@ def update_database():
 	hook_download = weechat.hook_process(
 			"python -c \"\n"
 			"import urllib2, zipfile, os\n"
+			"from sys import stderr\n"
 			"try:\n"
 			"	temp = os.path.join('%(script_dir)s', 'temp.zip')\n"
-			"	zip = urllib2.urlopen('%(url)s')\n"
+			"	zip = urllib2.urlopen('%(url)s', timeout=1)\n"
 			"	fd = open(temp, 'w')\n"
 			"	fd.write(zip.read())\n"
 			"	fd.close()\n"
+			"	print 'Download complete, uncompressing...'\n"
 			"	zip = zipfile.ZipFile(temp)\n"
 			"	zip.extractall(path='%(script_dir)s')\n"
 			"	os.remove(temp)\n"
 			"except Exception, e:\n"
-			"	print e\n\"" %{'url':database_url, 'script_dir':script_dir},
+			"	print >>stderr, e\n\"" %{'url':database_url, 'script_dir':script_dir},
 			timeout, 'update_database_cb', '')
 
-hook_stdout = ''
+process_stderr = ''
 def update_database_cb(data, command, rc, stdout, stderr):
-	global hook_download, hook_stdout
-	if stderr:
-		hook_stdout += stderr
+	global hook_download, process_stderr
+	#debug("%s @ stderr: '%s', stdout: '%s'" %(rc, stderr.strip('\n'), stdout.strip('\n')))
 	if stdout:
-		hook_stdout += stdout
+		say(stdout)
+	if stderr:
+		process_stderr += stderr
 	if int(rc) >= 0:
-		if hook_stdout:
-			error("There was an error...")
-			error(hook_stdout)
-			hook_stdout = ''
+		if process_stderr:
+			error(process_stderr)
+			process_stderr = ''
 		else:
-			say("Success.")
+			say('Success.')
 		hook_download = ''
-	return weechat.WEECHAT_RC_OK
+	return WEECHAT_RC_OK
 
 def is_ip(ip):
 	if ip.count('.') == 3:
@@ -106,8 +121,19 @@ def is_host(host):
 		return True
 	return False
 
-def is_nick(nick, buffer):
-	pass
+def get_host_by_nick(nick, buffer):
+	channel = weechat.buffer_get_string(buffer, 'localvar_channel')
+	server = weechat.buffer_get_string(buffer, 'localvar_server')
+	if channel and server:
+		infolist = weechat.infolist_get('irc_nick', '', '%s,%s' %(server, channel))
+		if infolist:
+			while weechat.infolist_next(infolist):
+				name = weechat.infolist_string(infolist, 'name')
+				if nick == name:
+					host = weechat.infolist_string(infolist, 'host')
+					return host[host.find('@')+1:] # strip everything in front of '@'
+			weechat.infolist_free(infolist)
+	return ''
 
 def get_ip(host):
 	import socket
@@ -128,7 +154,7 @@ def search_in_database(n):
 		while True:
 			mid = (max + min)/2
 			fd.seek(mid)
-			fd.readline()
+			fd.readline() # move cursor to next line
 			_, _, low, high, code, country = reader.next()
 			if low == last_low and high == last_high:
 				break
@@ -141,8 +167,6 @@ def search_in_database(n):
 			else:
 				break
 			last_low, last_high = low, high
-	except IOError, e:
-		error(e)
 	except StopIteration:
 		pass
 	return (None, None)
@@ -165,11 +189,24 @@ def cmd_country(data, buffer, args):
 	if not args:
 		return WEECHAT_RC_OK
 	if ' ' in args:
-		host = args[args.rfind(' '):]
-	else:
-		host = args
-	code, country = get_country(host)
-	whois('%s (%s)' %(country, code), host, buffer)
+		# picks the first argument only
+		args = args[:args.find(' ')]
+	if args == 'update':
+		update_database()
+		return WEECHAT_RC_OK
+	#debug('args: %s' %args)
+	try:
+		if not is_host(args):
+			# maybe a nick
+			host = get_host_by_nick(args, buffer)
+			#debug('host: %s' %host)
+		else:
+			host = args
+		code, country = get_country(host)
+		whois('%s (%s)' %(country, code), args, buffer)
+	except IOError:
+		error("IP database not found. You must download a database with '/country update' before "
+				"using this script.", buffer=buffer)
 	return WEECHAT_RC_OK
 
 ### signal callback
@@ -177,20 +214,24 @@ def whois_cb(data, signal, signal_data):
 	nick, user, host = signal_data.split()[3:6]
 	server = signal[:signal.find(',')]
 	#debug('%s | %s | %s' %(data, signal, signal_data))
-	code, country = get_country(host)
-	if code:
-		buffer = weechat.buffer_search('irc', 'server.%s' %server)
-		whois('%s (%s)' %(country, code), nick, buffer)
+	try:
+		code, country = get_country(host)
+		if code:
+			buffer = weechat.buffer_search('irc', 'server.%s' %server)
+			whois('%s (%s)' %(country, code), nick, buffer)
+	except IOError:
+		pass # no database installed
 	return WEECHAT_RC_OK
 
-if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, '', '', '', '', ''):
-	ip_database = os.path.join(get_script_dir(), database_file)
-	if not os.path.isfile(ip_database):
-		say("IP database not found.")
-		update_database()
+### main
+if import_ok and weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
+		SCRIPT_DESC, '', ''):
+	weechat.hook_signal('*,irc_in2_311', 'whois_cb', '')
+	weechat.hook_command('country', '', "nick|ip|uri", "", 'update||%(nick)', 'cmd_country', '')
+	if not check_database():
+		say("IP database not found. You must download a database with '/country update' before "
+				"using this script.")
 	else:
 		say("IP database found.")
-	weechat.hook_signal('*,irc_in2_311', 'whois_cb', '')
-	weechat.hook_command('country', '', "nick|host", "", '', 'cmd_country', '')
 
 # vim:set shiftwidth=4 tabstop=4 noexpandtab textwidth=100:
