2f59090dd1dd9f2a534cacd6a43b3f6e35b046ff
Elián Hanisch <lambdae2@gmail.com>
2010-01-10 20:51:24 -0300

    updated copyright year

--- python/grep.py
+++ python/grep.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ###
-# Copyright (c) 2009 by Elián Hanisch <lambdae2@gmail.com>
+# Copyright (c) 2009-2010 by Elián Hanisch <lambdae2@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
