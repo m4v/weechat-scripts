e52fa765c5967517bad622f9c6d3884e613e633b
Elián Hanisch <lambdae2@gmail.com>
2010-01-17 15:16:56 -0300

    grep: using tempfile module didn't work for python older than 2.6

--- python/grep.py
+++ python/grep.py
@@ -859,7 +859,7 @@ def show_matching_lines():
 
 # defined here for commodity
 grep_proccess_cmd = """python -c "
-import sys, tempfile, cPickle
+import sys, cPickle
 sys.path.append('%(home)s/python') # add WeeChat script dir so we can import grep
 sys.path.append('%(home)s/python/autoload')
 from grep import make_regexp, grep_file, strip_home, SCRIPT_VERSION
@@ -874,8 +874,10 @@ try:
         lines = grep_file(log, %(head)s, %(tail)s, %(after_context)s, %(before_context)s,
         %(count)s, regexp, '%(hilight)s', %(exact)s)
         d[log_name] = lines
-    fd = tempfile.NamedTemporaryFile('wb', delete=False)
-    print fd.name
+    #fd = tempfile.NamedTemporaryFile('wb', delete=False)
+    fdname = '/tmp/grep_search.tmp'
+    fd = open(fdname, 'wb')
+    print fdname
     cPickle.dump(d, fd, -1)
     fd.close()
 except Exception, e:
